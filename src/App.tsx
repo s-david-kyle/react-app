b86ed778c6ab90516268c8f0601219773008ee26
S. David Kyle <sdavidkyle@gmail.com>
2024-02-26 17:25:18 -0700

    Refactor code for improved performance and readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
-import Button from "./components/Button";
+import ListGroup from "./components/ListGroup";
 
 function App() {
+  let items = [
+    "New York",
+    "Los Angeles",
+    "Chicago",
+    "Houston",
+    "Phoenix",
+    "Philadelphia",
+    "San Antonio",
+  ];
+  let heading = "Cities";
+
+  const handleSelectItem = (item: string) => {
+    console.log("Selected item:", item);
+  };
+
   return (
-    <>
-      <div>
-        <Button buttonType="primary" buttonText="Click me" />
-      </div>
-    </>
+    <div>
+      <ListGroup
+        items={items}
+        heading={heading}
+        onSelectItem={handleSelectItem}
+      />
+    </div>
   );
 }
 
