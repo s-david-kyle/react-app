ba119eb084d67e2e8a955d6e83954f8089765d5b
S. David Kyle <sdavidkyle@gmail.com>
2024-03-18 13:21:22 -0700

    Adding form.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,9 @@
-import ListGroup from "./components/ListGroup";
-import Like from "./components/Like/Like";
-import ExpandableText from "./components/ExpandableText";
+import Form from "./components/Form";
 
 function App() {
-  let items = [
-    "New York",
-    "Los Angeles",
-    "Chicago",
-    "Houston",
-    "Phoenix",
-    "Philadelphia",
-    "San Antonio",
-  ];
-  let heading = "Cities";
-
-  const handleSelectItem = (item: string) => {
-    console.log("Selected item:", item);
-  };
-
   return (
     <div>
-      <Like />
-      <ListGroup
-        items={items}
-        heading={heading}
-        onSelectItem={handleSelectItem}
-      />
-      <ExpandableText numberOfChars={10}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </ExpandableText>
+      <Form />
     </div>
   );
 }
---
+++ src/components/Form.tsx
@@ -0,0 +1,30 @@
+import { FormEvent } from "react";
+
+const Form = () => {
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log("Form submitted!");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="name" className="form-label">
+          Name
+        </label>
+        <input id="name" className="form-control"></input>
+      </div>
+      <div className="mb-3">
+        <label htmlFor="age" className="form-label">
+          Age
+        </label>
+        <input id="age" type="number" className="form-control" />
+      </div>
+      <button className="btn btn-primary" type="submit">
+        Submit
+      </button>
+    </form>
+  );
+};
+
+export default Form;
