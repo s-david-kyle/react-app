7a34322516e3e2c1e07b78ae3f2a8205c5edce44
S. David Kyle <sdavidkyle@gmail.com>
2024-03-18 13:38:10 -0700

    Adding form fields

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,16 @@
-import { FormEvent } from "react";
+import { FormEvent, useRef } from "react";
 
 const Form = () => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const ageRef = useRef<HTMLInputElement>(null);
+  const person = { name: "", age: 0 };
+
   const handleSubmit = (event: FormEvent) => {
     event.preventDefault();
-    console.log("Form submitted!");
+    if (nameRef.current !== null) person.name = nameRef.current.value;
+    if (ageRef.current !== null) person.age = parseInt(ageRef.current.value);
+
+    console.log(person);
   };
 
   return (
@@ -12,13 +19,13 @@ const Form = () => {
         <label htmlFor="name" className="form-label">
           Name
         </label>
-        <input id="name" className="form-control"></input>
+        <input ref={nameRef} id="name" className="form-control"></input>
       </div>
       <div className="mb-3">
         <label htmlFor="age" className="form-label">
           Age
         </label>
-        <input id="age" type="number" className="form-control" />
+        <input ref={ageRef} id="age" type="number" className="form-control" />
       </div>
       <button className="btn btn-primary" type="submit">
         Submit
