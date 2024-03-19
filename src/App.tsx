5a97fdf9c47287009433e0c8948664e2c6726682
S. David Kyle <sdavidkyle@gmail.com>
2024-03-19 16:18:26 -0700

    Adding grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import Form from "./components/Form";
+import ExpenseList from "./components/expense-tracker/ExpenseList";
 
 function App() {
+  const expenses = [
+    { id: 1, description: "aaa", amount: 100, category: "Utilities" },
+    { id: 2, description: "qwea", amount: 100, category: "Utilities" },
+    { id: 3, description: "aqwea", amount: 100, category: "Utilities" },
+    { id: 4, description: "asdga", amount: 100, category: "Utilities" },
+    { id: 5, description: "asdgsda", amount: 100, category: "Utilities" },
+  ];
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log("Delete", id)}
+      />
     </div>
   );
 }
