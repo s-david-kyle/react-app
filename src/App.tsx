6f5501e9a1d8fcb5ed02e971e604d1f089378b71
S. David Kyle <sdavidkyle@gmail.com>
2024-06-10 11:10:35 -0700

    initial expense stuff

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const expenses = [
+    { id: 1, description: "Rent", amount: 1600, category: "Housing" },
+    { id: 2, description: "Groceries", amount: 100, category: "Food" },
+    { id: 3, description: "Utilities", amount: 150, category: "Utilities" },
+  ];
+
   return (
     <div>
-      <Form></Form>
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log("Delete: " + id)}
+      ></ExpenseList>
     </div>
   );
 }
